b2b9f2c6bc2f04be2a6b56ef6ddfdabd3843fd62
012e <huyphmnhat@gmail.com>
2024-10-24 22:45:19 +0700

    init a default admin user on run

---
+++ backend/score_manager/management/commands/initsuperuser.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        User.objects.create_superuser(
+            username="admin", password="admin", email="admin@admin.com"
+        )
