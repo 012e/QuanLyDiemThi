d06dc25b8e78f0acf6e41d00e4604c4017015d60
012e <huyphmnhat@gmail.com>
2024-10-24 22:45:42 +0700

    basic setup for frontend

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,30 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { ApiModule, BASE_PATH } from './core/api';
+import { provideHttpClient, withXsrfConfiguration } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimations(),
+    provideHttpClient(
+      withXsrfConfiguration({
+        cookieName: 'csrftoken',
+        headerName: 'X-CSRFToken',
+      }),
+    ),
+    importProvidersFrom(ApiModule),
+    {
+      provide: BASE_PATH,
+      useValue: 'http://localhost:8000',
+    },
+  ],
 };
---
+++ frontend/src/app/login/components/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { AuthService, Login } from '../../../core/api';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  public username: string = '';
+  public email: string = '';
+  public password: string = '';
+  constructor(private authService: AuthService) {}
+
+  public handleLogin(): void {
+    const loginInfo: Login = {
+      username: this.username,
+      email: this.email,
+      password: this.password,
+    };
+    this.authService.authLoginCreate(loginInfo).subscribe({
+      next: (response) => {
+        localStorage.setItem('acces_token', response.access);
+        localStorage.setItem('refresh_token', response.refresh);
+        alert('Login successful');
+      },
+      error: (error) => {
+        alert('Login failed: ' + error.message);
+      },
+    });
+  }
+}
