ddd71e459f1406e7af4b8e4e3c6436da59eb1c2a
012e <huyphmnhat@gmail.com>
2024-10-27 21:16:48 +0700

    ignore expired token in jwt interceptor

--- frontend/src/app/core/interceptors/jwt.interceptor.ts
+++ frontend/src/app/core/interceptors/jwt.interceptor.ts
@@ -1,8 +1,23 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { jwtDecode } from 'jwt-decode';
+
+function tokenExpired(token: string): boolean {
+  if (!token) return true;
+  try {
+    const decodedToken = jwtDecode(token);
+    if (!decodedToken.exp) return true;
+    const currentTime = Date.now() / 1000;
+    return decodedToken.exp < currentTime;
+  } catch (error) {
+    console.error('Error decoding token:', error);
+    return true;
+  }
+}
 
 export const JwtInterceptor: HttpInterceptorFn = (request, next) => {
   const token = localStorage.getItem('access_token');
-  if (token) {
+  if (!token) return next(request);
+  if (token && !tokenExpired(token)) {
     request = request.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`,
