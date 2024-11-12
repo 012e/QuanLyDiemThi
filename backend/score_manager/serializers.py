1421976c5b9ee3235b16b943530d48111869e254
012e <huyphmnhat@gmail.com>
2024-11-12 23:49:27 +0700

    humanize test

--- backend/score_manager/serializers.py
+++ backend/score_manager/serializers.py
@@ -1,15 +1,22 @@
+from typing import override
+
+import humanize
 import rest_framework.serializers as serializers
-from .models import Student, Class, Question, Subject, Difficulty, Test, Result
+
+from .models import Class, Difficulty, Question, Result, Student, Subject, Test
+
 
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
-        fields = '__all__'
+        fields = "__all__"
+
 
 class ClassSerializer(serializers.ModelSerializer):
     class Meta:
         model = Class
-        fields = '__all__'
+        fields = "__all__"
+
 
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
@@ -18,27 +25,34 @@ class QuestionSerializer(serializers.ModelSerializer):
 
 
 class SubjectSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Subject
         fields = "__all__"
 
 
 class DifficultySerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Difficulty
         fields = "__all__"
 
 
 class TestSerializer(serializers.ModelSerializer):
-    questions = serializers.PrimaryKeyRelatedField(queryset=Question.objects.all(), many=True)
+    questions = serializers.PrimaryKeyRelatedField(
+        queryset=Question.objects.all(), many=True
+    )
+
+    @override
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation["duration"] = humanize.precisedelta(instance.duration)
+        return representation
 
     class Meta:
         model = Test
-        fields = '__all__'  
-        
+        fields = "__all__"
+
+
 class ResultSerializer(serializers.ModelSerializer):
     class Meta:
         model = Result
-        fields = '__all__' 
+        fields = "__all__"
