b9fe65aeebcda234ef9742359024a5e106ee235c
012e <huyphmnhat@gmail.com>
2024-11-21 22:31:22 +0700

    add basic user info page

--- frontend/src/app/core/services/auth.service.ts
+++ frontend/src/app/core/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { decodeJwt } from 'jose';
+import { UserRole } from '../enums/user-role';
 
 @Injectable({
   providedIn: 'root',
@@ -22,6 +23,29 @@ export class AuthService {
     const token = localStorage.getItem('access_token');
     if (!token || this.tokenExpired(token)) return false;
     return true;
+  }
+
+  public getRole(): UserRole {
+    const token = localStorage.getItem('access_token');
+    try {
+      if (!token) throw new Error('Token not found');
+      const decodedToken = decodeJwt(token);
+      if (!decodedToken['role']) throw new Error('Role not found in token');
+      const role = decodedToken['role'].toString();
+      if (role !== 'user' && role !== 'staff' && role !== 'admin') {
+        throw new Error('Invalid role in token');
+      }
+      if (role === 'user') return UserRole.User;
+      if (role === 'staff') return UserRole.Staff;
+      return UserRole.Admin;
+    } catch (e) {
+      console.error(`Error decoding token: ${e}. Defaulting to user role.`);
+      return UserRole.User;
+    }
+  }
+
+  public logout(): void {
+    localStorage.removeItem('access_token');
   }
 
   public tryRefreshToken(): boolean {
---
+++ frontend/src/app/user/user.component.ts
@@ -0,0 +1,131 @@
+import { Component } from '@angular/core';
+import { DividerModule } from 'primeng/divider';
+import { UserRole } from '../core/enums/user-role';
+import { AuthService } from '../core/services/auth.service';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { MessageService } from 'primeng/api';
+import { AuthService as ApiAuthService, UserDetails } from '../core/api';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [DividerModule, InputTextModule, ButtonModule, ReactiveFormsModule],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  public userRole!: string;
+  public userDetails!: UserDetails;
+  public userDetailsForm!: FormGroup;
+  public editing = false;
+
+  public constructor(
+    private readonly authService: AuthService,
+    private readonly messageService: MessageService,
+    private readonly authApiService: ApiAuthService,
+    private readonly fb: FormBuilder,
+    private readonly router: Router,
+  ) {}
+
+  public roleToString(role: UserRole): string {
+    switch (role) {
+      case UserRole.User:
+        return 'User';
+      case UserRole.Staff:
+        return 'Staff';
+      case UserRole.Admin:
+        return 'Admin';
+      default:
+        return 'Unknown';
+    }
+  }
+
+  public logout(): void {
+    this.authService.logout();
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Logged out',
+      detail: 'You have been logged out successfully.',
+    });
+    this.router.navigate(['/auth/login']);
+  }
+
+  public initForm(): void {
+    this.userDetailsForm = this.fb.group({
+      username: [''],
+      email: [''],
+      firstName: [''],
+      lastName: [''],
+    });
+    this.userDetailsForm.disable();
+  }
+
+  public ngOnInit(): void {
+    this.initForm();
+    this.userRole = this.roleToString(this.authService.getRole());
+
+    this.authApiService.authUserRetrieve().subscribe({
+      next: (userDetails) => {
+        this.userDetails = userDetails;
+        this.userDetailsForm.patchValue({
+          username: userDetails.username,
+          email: userDetails.email,
+          firstName: userDetails.last_name,
+          lastName: userDetails.first_name,
+        });
+      },
+      error: (error) => {
+        console.error('Error retrieving user details:', error);
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error retrieving user details',
+          detail: 'An error occurred while retrieving user details.',
+        });
+      },
+    });
+  }
+
+  public enableEdit() {
+    this.editing = true;
+    this.userDetailsForm.enable();
+    this.userDetailsForm.get('email')?.disable();
+  }
+
+  public saveChanges() {
+    this.editing = false;
+    this.authApiService
+      .authUserPartialUpdate({
+        username: this.userDetailsForm.get('username')?.value,
+        first_name: this.userDetailsForm.get('firstName')?.value,
+        last_name: this.userDetailsForm.get('lastName')?.value,
+      })
+      .subscribe({
+        next: (userDetails) => {
+          this.userDetails = userDetails;
+          this.userDetailsForm.patchValue({
+            username: userDetails.username,
+            email: userDetails.email,
+            firstName: userDetails.last_name,
+            lastName: userDetails.first_name,
+          });
+          this.messageService.add({
+            severity: 'success',
+            summary: 'User details updated',
+            detail: 'User details have been updated successfully.',
+          });
+        },
+        error: (error) => {
+          console.error('Error updating user details:', error);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error updating user details',
+            detail: 'An error occurred while updating user details.',
+          });
+        },
+      });
+    this.userDetailsForm.disable();
+  }
+}
