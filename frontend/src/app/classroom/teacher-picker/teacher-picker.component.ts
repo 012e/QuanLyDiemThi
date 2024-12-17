e2e647a5e58d8b00f1b9c993e05d475f3969048b
Nguyen Thanh Tuan <nguyentuan02156@gmail.com>
2024-12-18 00:44:07 +0700

    add teacher picker

--- frontend/src/app/classroom/teacher-picker/teacher-picker.component.ts
+++ frontend/src/app/classroom/teacher-picker/teacher-picker.component.ts
@@ -1,12 +1,124 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import {
+  DialogService,
+  DynamicDialogComponent,
+  DynamicDialogRef,
+} from 'primeng/dynamicdialog';
+import { TableModule, TablePageEvent } from 'primeng/table';
+import { Subject as RxSubject, debounceTime, distinctUntilChanged } from 'rxjs';
+import { User, UserService } from '../../core/api';
+import { InputTextModule } from 'primeng/inputtext';
 @Component({
   selector: 'app-teacher-picker',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, TableModule, InputTextModule],
   templateUrl: './teacher-picker.component.html',
-  styleUrl: './teacher-picker.component.css'
+  styleUrl: './teacher-picker.component.css',
 })
-export class TeacherPickerComponent {
+export class TeacherPickerComponent implements OnInit {
+  public instance: DynamicDialogComponent | undefined;
+  public exceptTeacher!: User;
 
+  constructor(
+    public dialogRef: DynamicDialogRef,
+    private readonly dialogService: DialogService,
+    private readonly teacherService: UserService,
+    private readonly messageService: MessageService
+  ) {
+    this.instance = this.dialogService.getInstance(this.dialogRef);
+  }
+
+  private readonly DEFAULT_PAGE_SIZE = 10;
+
+  teachers!: User[];
+
+  submitted: boolean = false;
+  searchValue: string | undefined;
+
+  public count!: number;
+  public first: number = 0;
+  public rows: number = this.DEFAULT_PAGE_SIZE;
+  public searchText: string = '';
+
+  private searchText$ = new RxSubject<string>();
+
+  public ngOnInit(): void {
+    this.loadInitialData();
+    this.initParams();
+  }
+
+  public initParams(): void {
+    if (!this.instance) {
+      throw new Error('Instance is not defined');
+    }
+    const data = this.instance.data;
+    if (!data) {
+      throw new Error('Data is not defined');
+    }
+    this.exceptTeacher = data.exceptTeacher;
+  }
+
+  public resetPage() {
+    this.first = 0;
+  }
+
+  private loadInitialData() {
+    this.searchText$
+      .pipe(debounceTime(300), distinctUntilChanged())
+      .subscribe((query: string) => {
+        this.resetPage();
+        this.searchText = query;
+        this.updatePage();
+      });
+
+    this.updatePage();
+  }
+
+  public onPage(event: TablePageEvent): void {
+    this.first = event.first;
+    this.rows = event.rows;
+    this.updatePage();
+  }
+
+  public updatePage(): void {
+    this.teacherService
+      .userList(this.rows, this.first, undefined, this.searchText)
+      .subscribe((data) => {
+        this.teachers = data.results;
+        this.count = data.count;
+      });
+  }
+
+  public showError(error: string) {
+    this.messageService.add({
+      severity: 'error',
+      summary: 'Error',
+      detail: error,
+      life: 3000,
+    });
+  }
+
+  public showSuccess(message: string) {
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Successful',
+      detail: message,
+      life: 3000,
+    });
+  }
+
+  public getValue(event: Event) {
+    return (event.target as HTMLInputElement).value;
+  }
+
+  public handleSearch(query: string) {
+    this.searchText$.next(query);
+  }
+
+  public select(teacher: User): void {
+    console.log(`Selected teacher ${teacher.id}`);
+    this.dialogRef.close(teacher);
+  }
 }
