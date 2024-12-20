c9a8ad77738a5721c1f081e2de6038e40a7b69a4
Nguyen Thanh Tuan <nguyentuan02156@gmail.com>
2024-12-21 01:02:56 +0700

    temp

---
+++ frontend/src/app/classroom/student-picker/student-picker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-picker',
+  standalone: true,
+  imports: [],
+  templateUrl: './student-picker.component.html',
+  styleUrl: './student-picker.component.css'
+})
+export class StudentPickerComponent {
+
+}
