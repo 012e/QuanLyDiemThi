d95f94763c8539e99459ff905e60a07cb06e8938
012e <huyphmnhat@gmail.com>
2024-12-22 17:22:32 +0700

    feat: allow changing summary year

--- frontend/src/app/summary/summary.component.ts
+++ frontend/src/app/summary/summary.component.ts
@@ -1,25 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { SummaryRetrieve200Response, SummaryService } from '../core/api';
 import { MessageService } from 'primeng/api';
 import { TableModule } from 'primeng/table';
 import { DecimalPipe } from '@angular/common';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputNumber, InputNumberInputEvent, InputNumberModule } from 'primeng/inputnumber';
+import { FormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-summary',
   standalone: true,
-  imports: [TableModule, DecimalPipe],
+  imports: [TableModule, DecimalPipe, InputNumberModule, FormsModule],
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.css',
 })
 export class SummaryComponent implements OnInit {
   public year: number = new Date().getFullYear();
   public data: SummaryRetrieve200Response | undefined;
+  public year$: Subject<number> = new Subject<number>();
 
   constructor(
     private readonly route: ActivatedRoute,
     private readonly summarySerivce: SummaryService,
     private readonly messageService: MessageService,
+    private readonly router: Router,
   ) {}
 
   public updateYearData(year: number): void {
@@ -46,7 +52,26 @@ export class SummaryComponent implements OnInit {
       } else {
         this.year = new Date().getFullYear();
       }
+      this.messageService.add({
+        severity: 'info',
+        summary: 'Info',
+        detail: `Updated summary for year ${this.year}.`,
+
+      });
       this.updateYearData(this.year);
     });
+
+    this.year$
+      .pipe(debounceTime(500), distinctUntilChanged())
+      .subscribe((year) => {
+        this.router.navigate(['/summary', year]);
+      });
+  }
+
+  public updateYear(event: InputNumberInputEvent): void {
+    if (!event.value) {
+      return;
+    }
+    this.year$.next(+event.value);
   }
 }
