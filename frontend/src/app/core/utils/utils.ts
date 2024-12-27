4b0973d08c2e6534c61f1939c9cc395df702a250
012e <huyphmnhat@gmail.com>
2024-12-28 02:21:34 +0700

    improve error utils: allow error message

--- frontend/src/app/core/utils/utils.ts
+++ frontend/src/app/core/utils/utils.ts
@@ -11,18 +11,14 @@ export class Utils {
   }
   static prettyError(error: object): string {
     let result = '';
-    if ("detail" in error) {
-      return error["detail"] as string;
-    }
     for (const key in error) {
       if (error.hasOwnProperty(key)) {
         result += `${Utils.titleCase(key)}: `;
 
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
-        const allErrors = error[key];
+        const allErrors = Array.isArray(error[key]) ? error[key] : [error[key]];
 
-        console.log("what", allErrors);
         for (let i = 0; i < allErrors.length; i++) {
           console.log("yaya:w ", allErrors[i]);
           result += `\t${allErrors[i]}\n`;
