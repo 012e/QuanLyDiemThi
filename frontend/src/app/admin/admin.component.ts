3076b2d212fb6851fdc5b104f83bc70f197c075d
Nguyen Thanh Tuan <nguyentuan02156@gmail.com>
2024-12-28 18:20:43 +0700

    add config logic

--- frontend/src/app/admin/admin.component.ts
+++ frontend/src/app/admin/admin.component.ts
@@ -97,9 +97,26 @@ export class AdminComponent implements OnInit {
           return false;
         }
         return true;
+      case 'MAX_TEST_SCORE':
+        const minTestScoreConfig = this.configs.find(
+          (c) => c.key === 'MIN_TEST_SCORE'
+        );
+        if (
+          minTestScoreConfig &&
+          Number(config.value) < Number(minTestScoreConfig.value)
+        ) {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: 'MAX_TEST_SCORE must be greater than MIN_TEST_SCORE',
+            life: 3000,
+          });
+          return false;
+        }
+        return true;
       case 'MIN_TEST_DURATION':
         const maxTestDurationConfig = this.configs.find(
-          (c) => c.key === 'MAX_TEST_SCORE'
+          (c) => c.key === 'MAX_TEST_DURATION'
         );
         if (
           Number(config.value) < 0 ||
@@ -116,7 +133,60 @@ export class AdminComponent implements OnInit {
           return false;
         }
         return true;
-        
+      case 'MAX_TEST_DURATION':
+        const minTestDurationConfig = this.configs.find(
+          (c) => c.key === 'MIN_TEST_DURATION'
+        );
+        if (
+          minTestDurationConfig &&
+          Number(config.value) < Number(minTestDurationConfig.value)
+        ) {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: 'MAX_TEST_DURATION must be greater than MIN_TEST_DURATION',
+            life: 3000,
+          });
+          return false;
+        }
+        return true;
+      case 'MIN_QUESTIONS_PER_TEST':
+        const maxQuestionsPerTestConfig = this.configs.find(
+          (c) => c.key === 'MAX_QUESTIONS_PER_TEST'
+        );
+        if (
+          Number(config.value) < 0 ||
+          (maxQuestionsPerTestConfig &&
+            Number(config.value) > Number(maxQuestionsPerTestConfig.value))
+        ) {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail:
+              'MIN_QUESTIONS_PER_TEST must be greater than 1 and less than MAX_QUESTIONS_PER_TEST',
+            life: 3000,
+          });
+          return false;
+        }
+        return true;
+      case 'MAX_QUESTIONS_PER_TEST':
+        const minQuestionsPerTestConfig = this.configs.find(
+          (c) => c.key === 'MIN_QUESTIONS_PER_TEST'
+        );
+        if (
+          minQuestionsPerTestConfig &&
+          Number(config.value) < Number(minQuestionsPerTestConfig.value)
+        ) {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail:
+              'MAX_QUESTIONS_PER_TEST must be greater than MIN_QUESTIONS_PER_TEST',
+            life: 3000,
+          });
+          return false;
+        }
+        return true;
       default:
         // handle the default case
         return true;
