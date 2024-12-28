740f7fda36282eb2b15a451e66b899a94b015adb
012e <huyphmnhat@gmail.com>
2024-12-28 22:37:33 +0700

    --wip--

--- backend/score_manager/models.py
+++ backend/score_manager/models.py
@@ -1,8 +1,32 @@
-from django.contrib.auth.models import User
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator
+from django.db import models
 
-User._meta.get_field("email")._unique = True  # pyright: ignore
+
+class Permission(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Role(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField(blank=True, null=True)
+    permissions = models.ManyToManyField(Permission, related_name="roles")
+
+    def __str__(self):
+        return self.name
+
+
+class CustomUser(AbstractUser):
+    roles = models.ManyToManyField(Role, related_name="users")
+
+    def __str__(self):
+        return self.username
+
+
+CustomUser._meta.get_field("email")._unique = True  # pyright: ignore
 
 
 class Subject(models.Model):
@@ -17,7 +41,7 @@ class Subject(models.Model):
 
 class Class(models.Model):
     name = models.CharField(max_length=100)
-    teacher = models.ForeignKey(User, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.name)
@@ -26,8 +50,7 @@ class Class(models.Model):
         ordering = ["name"]
         constraints = [
             models.UniqueConstraint(
-                fields=["name", "teacher"],
-                name="unique_class_name_teacher"
+                fields=["name", "teacher"], name="unique_class_name_teacher"
             )
         ]
 
@@ -35,7 +58,9 @@ class Class(models.Model):
 class Student(models.Model):
     name = models.CharField(max_length=100)
     student_code = models.CharField(max_length=10, blank=False, unique=True)
-    classroom = models.ForeignKey(Class, null=True, on_delete=models.SET_NULL, db_index=True)
+    classroom = models.ForeignKey(
+        Class, null=True, on_delete=models.SET_NULL, db_index=True
+    )
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     updated_at = models.DateTimeField(auto_now=True, db_index=True)
 
@@ -55,6 +80,7 @@ class Difficulty(models.Model):
     class Meta:
         ordering = ["name"]
 
+
 class Question(models.Model):
     detail = models.TextField(blank=False, null=True)
     difficulty = models.ForeignKey(Difficulty, null=False, on_delete=models.CASCADE)
@@ -71,18 +97,21 @@ class Question(models.Model):
         ordering = ["updated_at"]
         constraints = [
             models.UniqueConstraint(
-                fields=["detail", "subject"],
-                name="unique_question_detail_subject"
+                fields=["detail", "subject"], name="unique_question_detail_subject"
             )
         ]
 
 
 class Test(models.Model):
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE, db_index=True)
-    semester = models.PositiveSmallIntegerField(null=False, blank=False,validators=[MinValueValidator(0)])
+    semester = models.PositiveSmallIntegerField(
+        null=False, blank=False, validators=[MinValueValidator(0)]
+    )
     datetime = models.DateTimeField()
     duration = models.DurationField()
-    questions = models.ManyToManyField(Question, blank=True, through="QuestionInTestModel")
+    questions = models.ManyToManyField(
+        Question, blank=True, through="QuestionInTestModel"
+    )
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     updated_at = models.DateTimeField(auto_now=True, db_index=True)
 
@@ -92,30 +121,32 @@ class Test(models.Model):
     class Meta:
         ordering = ["updated_at"]
 
+
 class QuestionInTestModel(models.Model):
     test = models.ForeignKey(Test, on_delete=models.CASCADE, db_index=True, blank=False)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, db_index=True, blank=False)
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE, db_index=True, blank=False
+    )
     order = models.PositiveSmallIntegerField(null=False)
+
     class Meta:
         ordering = ["order", "question"]
 
 
-
 class Result(models.Model):
     test = models.ForeignKey(Test, on_delete=models.CASCADE)
-    teacher = models.ForeignKey(User, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     classroom = models.ForeignKey(Class, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     updated_at = models.DateTimeField(auto_now=True, db_index=True)
+
     class Meta:
         constraints = [
             models.UniqueConstraint(
                 fields=["test", "teacher", "classroom"],
-                name="unique_test_teacher_classroom"
+                name="unique_test_teacher_classroom",
             )
         ]
-
-    class Meta:
         ordering = ["updated_at"]
 
 
