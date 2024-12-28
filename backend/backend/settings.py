33ed64dee03d31d2032cd22496964942a7ae5f3e
012e <huyphmnhat@gmail.com>
2024-12-28 19:02:25 +0700

    bug: can't start backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,7 +31,7 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "django.contrib.admin",
     "django.contrib.auth",
-    "django.contrib.contenttypes"
+    "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
